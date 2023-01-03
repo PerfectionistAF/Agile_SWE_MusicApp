dfe70c2c5b35062c3fd1dffc1a8e9f420c764dea
Sohayla Ihab <77901496+PerfectionistAF@users.noreply.github.com>
2023-01-03 02:46:04 +0200

    Add files via upload

---
+++ Team 5/KWA_/Models/Applicant.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KWA.Controllers;
+namespace KWA.Models
+{
+    public class Applicant
+    {
+        public string FirstName;
+        public string MiddleName;
+        public string LastName;
+        public string Email;
+        public string PhoneNumber;
+        public string DOB;
+        public bool Gender;
+        public int NationalId;
+        public string College;
+        public string Faculty;
+        public int YearOfStudy;
+        public bool Disability;
+        public string InstrumentChoice;
+        public byte[] NationaIdImage;
+        public byte[] CollegeIdImage;
+        public byte[] PersonalImage;
+    }
+}
---
+++ Team 5/KWA_/Scripts/dbo.Table.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[FirstName] NVARCHAR(50) NOT NULL, 
+    [MiddleName] NVARCHAR(50) NULL, 
+    [LastName] NVARCHAR(50) NOT NULL, 
+    [Email] NVARCHAR(MAX) NOT NULL, 
+    [PhoneNumber] NUMERIC(11) NOT NULL, 
+    [DOB] DATE NOT NULL, 
+    [Gender] BIT NOT NULL, 
+    [InstrumentChoice] NVARCHAR(50) NOT NULL, 
+    [NationalId] NUMERIC(14) NOT NULL, 
+    [College] NVARCHAR(50) NOT NULL, 
+    [Faculty] NVARCHAR(50) NOT NULL, 
+    [YearOfStudy] DATE NOT NULL, 
+    [Disability] BIT NOT NULL, 
+    [NationalIdImage] IMAGE NOT NULL, 
+    [CollegeIdImage] IMAGE NOT NULL, 
+    [PersonalImage] IMAGE NOT NULL, 
+    CONSTRAINT [PK_Table] PRIMARY KEY ([Email]) 
+)
